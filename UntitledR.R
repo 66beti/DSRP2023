c29dd79cc0e9a9f955e023dc26ebd640f51d0381
Betina Yama <betinayama3@gmail.com>
2023-07-10 15:21:23 -0400

    README

---
+++ UntitledR.R
@@ -0,0 +1,7 @@
+usethis::use_git_config(user.name = "Betina Yama",
+                        user.email ="betinayama3@gmail.com")
+git config --global user.name '66beti'                              
+install.packages("usethis")
+create_github_token()
+usethis::create_github_token()
+credentials::set_github_pat("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")
